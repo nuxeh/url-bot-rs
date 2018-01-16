872d984d9ae7fd658e23e501bcece7fc40b3cfa7
Edward Cragg <drq.11235@gmail.com>
2018-01-16 23:01:10 +0000

    add resolve url function

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 extern crate irc;
 extern crate hyper;
+extern crate tokio_core;
 
+use std::io::{self, Write};
 use irc::client::prelude::*;
-//use hyper::Client;
+use hyper::Client;
 
 /* Message { tags: None, prefix: Some("edcragg!edcragg@ip"), command: PRIVMSG("#music", "test") } */
 
 fn main() {
+
 	let server = IrcServer::new("config.toml").unwrap();
 	server.identify().unwrap();
 	server.for_each_incoming(|message| {
@@ -20,8 +23,8 @@ fn main() {
 
 					let url = msg.parse::<hyper::Uri>().unwrap();
 					match url.scheme() {
-						Some("http") => { }
-						Some("https") => { }
+						Some("http") => { resolve_url(url); }
+						Some("https") => { resolve_url(url); }
 						_ => {println!("This example only works with 'http' URLs.");}
 					}
 
@@ -33,4 +36,24 @@ fn main() {
 			_ => (),
 		}
 	}).unwrap()
+}
+
+fn resolve_url(url: hyper::Uri) {
+
+    let mut core = tokio_core::reactor::Core::new().unwrap();
+    let handle = core.handle();
+    let client = Client::new(&handle);
+
+    let work = client.get(url).and_then(|res| {
+        println!("Response: {}", res.status());
+        println!("Headers: \n{}", res.headers());
+
+        res.body().for_each(|chunk| {
+            io::stdout().write_all(&chunk).map_err(From::from)
+        })
+    }).map(|_| {
+        println!("\n\nDone.");
+    });
+
+    core.run(work).unwrap();
 }
