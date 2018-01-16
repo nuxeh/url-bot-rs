e75dbf9ab6f5963ceb4667609c4d22de9ec87e68
Edward Cragg <drq.11235@gmail.com>
2018-01-16 22:47:10 +0000

    check for http urls
    "http://youtube.com/"
    "https://youtube.com/"
    This example only works with 'http' URLs.
    "lemonade"
    This example only works with 'http' URLs.
    "http://youtube.com/"

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 extern crate irc;
+extern crate hyper;
 
 use irc::client::prelude::*;
+//use hyper::Client;
 
 /* Message { tags: None, prefix: Some("edcragg!edcragg@ip"), command: PRIVMSG("#music", "test") } */
 
@@ -9,21 +11,24 @@ fn main() {
     server.identify().unwrap();
     server.for_each_incoming(|message| {
 
-//    println!("{:?}", message);
-
     match message.command {
             Command::PRIVMSG(ref target, ref msg) => {
                 let tokens: Vec<_> = msg.split(' ').collect();
 
                 for t in tokens {
                     println!("{:?}", t);
+
+                    let url = msg.parse::<hyper::Uri>().unwrap();
+                    if url.scheme() != Some("http") {
+                        println!("This example only works with 'http' URLs.");
+                    }
+
+                    server.send_privmsg(
+                        message.response_target().unwrap_or(target), msg
+                    ).unwrap();
                 }
-                server.send_privmsg(
-                    message.response_target().unwrap_or(target), msg
-                ).unwrap();
             }
             _ => (),
         }
     }).unwrap()
-
 }
