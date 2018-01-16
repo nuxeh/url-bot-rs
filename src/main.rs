7e389e21bc948d3908639190f194a1a89de868a0
Edward Cragg <drq.11235@gmail.com>
2018-01-16 21:06:42 +0000

    add irc source

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+extern crate irc;
+
+use irc::client::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let server = IrcServer::new("config.toml").unwrap();
+    server.identify().unwrap();
+    server.for_each_incoming(|message| {
+	println!("{:?}", message);
+        // Do message processing.
+    }).unwrap()
 }
