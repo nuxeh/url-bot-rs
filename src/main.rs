b6d421dd4e9debc43b67a8c621b9a51009273118
Edward Cragg <drq.11235@gmail.com>
2018-01-16 22:28:23 +0000

    echo messages

--- src/main.rs
+++ src/main.rs
@@ -8,25 +8,22 @@ fn main() {
 	let server = IrcServer::new("config.toml").unwrap();
 	server.identify().unwrap();
 	server.for_each_incoming(|message| {
-	println!("{:?}", message);
+
+//	println!("{:?}", message);
+
 	match message.command {
 			Command::PRIVMSG(ref target, ref msg) => {
-				if msg.starts_with(server.current_nickname()) {
-					let tokens: Vec<_> = msg.split(' ').collect();
-					if tokens.len() > 2 {
-						let n = tokens[0].len() + tokens[1].len() + 2;
-						if let Ok(count) = tokens[1].parse::<u8>() {
-							for _ in 0..count {
-								server.send_privmsg(
-									message.response_target().unwrap_or(target),
-									&msg[n..]
-								).unwrap();
-							}
-						}
-					}
+				let tokens: Vec<_> = msg.split(' ').collect();
+
+				for t in tokens {
+					println!("{:?}", t);
 				}
+				server.send_privmsg(
+					message.response_target().unwrap_or(target), msg
+				).unwrap();
 			}
 			_ => (),
 		}
 	}).unwrap()
+
 }
