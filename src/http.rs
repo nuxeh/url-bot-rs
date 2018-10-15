7ae2994e6974b776ea4dfc58786aee6bf168fafb
Vadim Smirnov <smirnovvad7@gmail.com>
2018-10-15 04:51:02 +0300

    Fix #46 Incorrect image metadata

--- src/http.rs
+++ src/http.rs
@@ -1,15 +1,15 @@
+use config::ConfOpts;
+use failure::Error;
 use htmlescape::decode_html;
 use std::time::Duration;
+use humansize::{file_size_opts as options, FileSize};
+use image::{gif, jpeg, png, ImageDecoder};
 use itertools::Itertools;
+use mime::{Mime, HTML, IMAGE, TEXT};
 use regex::Regex;
-use failure::Error;
+use reqwest::header::{ACCEPT_LANGUAGE, CONTENT_LENGTH, CONTENT_TYPE, USER_AGENT};
 use reqwest::Client;
-use reqwest::header::{USER_AGENT, ACCEPT_LANGUAGE, CONTENT_TYPE, CONTENT_LENGTH};
 use std::io::Read;
-use immeta::{GenericMetadata, load_from_buf};
-use mime::{Mime, IMAGE, TEXT, HTML};
-use humansize::{FileSize, file_size_opts as options};
-use config::ConfOpts;
 
 const DOWNLOAD_SIZE: u64 = 100 * 1024; // 100kB
 
@@ -20,21 +20,28 @@ pub fn resolve_url(url: &str, lang: &str, conf: &ConfOpts) -> Result<String, Err
         .timeout(Duration::from_secs(10)) // per read/write op
         .build()?;
 
-    let resp = client.get(url)
+    let resp = client
+        .get(url)
         .header(USER_AGENT, "url-bot-rs/0.1")
         .header(ACCEPT_LANGUAGE, lang)
         .send()?
         .error_for_status()?;
 
     // Get some response headers
-    let content_type = resp.headers().get(CONTENT_TYPE)
+    let content_type = resp
+        .headers()
+        .get(CONTENT_TYPE)
         .and_then(|typ| typ.to_str().ok())
         .and_then(|typ| typ.parse::<Mime>().ok());
-    let len = resp.headers().get(CONTENT_LENGTH)
+    let len = resp
+        .headers()
+        .get(CONTENT_LENGTH)
         .and_then(|len| len.to_str().ok())
         .and_then(|len| len.parse().ok())
         .unwrap_or(0);
-    let size = len.file_size(options::CONVENTIONAL).unwrap_or(String::new());
+    let size = len
+        .file_size(options::CONVENTIONAL)
+        .unwrap_or(String::new());
 
     // Download body
     let mut body = Vec::new();
@@ -44,7 +51,7 @@ pub fn resolve_url(url: &str, lang: &str, conf: &ConfOpts) -> Result<String, Err
                 (IMAGE, _) => 10 * 1024 * 1024, // 10MB
                 _ => DOWNLOAD_SIZE,
             }
-        },
+        }
         None => DOWNLOAD_SIZE,
     };
     resp.take(bytes).read_to_end(&mut body)?;
@@ -52,15 +59,12 @@ pub fn resolve_url(url: &str, lang: &str, conf: &ConfOpts) -> Result<String, Err
 
     // Get title or metadata
     let title = match content_type {
-        Some(ct) => {
-            match (ct.type_(), ct.subtype()) {
-                (IMAGE, _) => parse_title(&contents)
-                    .or(get_image_metadata(&conf, &body))
-                    .or(get_mime(&conf, &ct, &size)),
-                (TEXT, HTML) => parse_title(&contents),
-                _ => parse_title(&contents)
-                    .or(get_mime(&conf, &ct, &size)),
-            }
+        Some(ct) => match (ct.type_(), ct.subtype()) {
+            (IMAGE, _) => parse_title(&contents)
+                .or(get_image_metadata(&conf, &body))
+                .or(get_mime(&conf, &ct, &size)),
+            (TEXT, HTML) => parse_title(&contents),
+            _ => parse_title(&contents).or(get_mime(&conf, &ct, &size)),
         },
         None => parse_title(&contents),
     }.ok_or_else(|| format_err!("failed to parse title"))?;
@@ -71,26 +75,22 @@ pub fn resolve_url(url: &str, lang: &str, conf: &ConfOpts) -> Result<String, Err
 fn get_mime(conf: &ConfOpts, c_type: &Mime, size: &str) -> Option<String> {
     match conf.report_mime {
         Some(true) => Some(format!("{} {}", c_type, size.replace(" ", ""))),
-        _ => None
+        _ => None,
     }
 }
 
 fn get_image_metadata(conf: &ConfOpts, body: &[u8]) -> Option<String> {
     if !conf.report_metadata.unwrap() {
-        return None;
-    };
-    if let Ok(img_meta) = load_from_buf(&body) {
-        return match img_meta {
-            GenericMetadata::Jpeg(m) => Some(format!("image/jpeg {}×{}",
-                m.dimensions.width, m.dimensions.height)),
-            GenericMetadata::Gif(m) => Some(format!("image/gif {}×{}",
-                m.dimensions.width, m.dimensions.height)),
-            GenericMetadata::Png(m) => Some(format!("image/png {}×{}",
-                m.dimensions.width, m.dimensions.height)),
-            _ => None,
-        };
-    };
-    None
+        None
+    } else if let Ok((w, h)) = jpeg::JPEGDecoder::new(body).dimensions() {
+        Some(format!("image/jpeg {}×{}", w, h))
+    } else if let Ok((w, h)) = png::PNGDecoder::new(body).dimensions() {
+        Some(format!("image/png {}×{}", w, h))
+    } else if let Ok((w, h)) = gif::Decoder::new(body).dimensions() {
+        Some(format!("image/gif {}×{}", w, h))
+    } else {
+        None
+    }
 }
 
 fn parse_title(page_contents: &str) -> Option<String> {
@@ -102,11 +102,7 @@ fn parse_title(page_contents: &str) -> Option<String> {
 
     // make any multi-line title string into a single line,
     // trim leading and trailing whitespace
-    let title_one_line = title_dec
-        .trim()
-        .lines()
-        .map(|line| line.trim())
-        .join(" ");
+    let title_one_line = title_dec.trim().lines().map(|line| line.trim()).join(" ");
 
     if title_one_line.is_empty() {
         return None;
@@ -133,24 +129,67 @@ mod tests {
         assert_eq!(None, parse_title(&"    ".to_string()));
         assert_eq!(None, parse_title(&"<title></title>".to_string()));
         assert_eq!(None, parse_title(&"<title>    </title>".to_string()));
-        assert_eq!(None,
-             parse_title(&"floofynips, not a real webpage".to_string()));
-        assert_eq!(Some("cheese is nice".to_string()),
-            parse_title(&"<title>cheese is nice</title>".to_string()));
-        assert_eq!(Some("squanch".to_string()),
-            parse_title(&"<title>     squanch</title>".to_string()));
-        assert_eq!(Some("squanch".to_string()),
-            parse_title(&"<title>squanch     </title>".to_string()));
-        assert_eq!(Some("squanch".to_string()),
-            parse_title(&"<title>\nsquanch</title>".to_string()));
-        assert_eq!(Some("squanch".to_string()),
-            parse_title(&"<title>\n  \n  squanch</title>".to_string()));
-        assert_eq!(Some("we like the moon".to_string()),
-            parse_title(&"<title>\n  \n  we like the moon</title>".to_string()));
-        assert_eq!(Some("&hello123&<>''~".to_string()),
-            parse_title(&"<title>&amp;hello123&amp;&lt;&gt;''~</title>".to_string()));
-        assert_eq!(Some("CVE - CVE-2018-11235".to_string()),
-            parse_title(&"<title>CVE -\nCVE-2018-11235\n</title>".to_string()));
+        assert_eq!(
+            None,
+            parse_title(&"floofynips, not a real webpage".to_string())
+        );
+        assert_eq!(
+            Some("cheese is nice".to_string()),
+            parse_title(&"<title>cheese is nice</title>".to_string())
+        );
+        assert_eq!(
+            Some("squanch".to_string()),
+            parse_title(&"<title>     squanch</title>".to_string())
+        );
+        assert_eq!(
+            Some("squanch".to_string()),
+            parse_title(&"<title>squanch     </title>".to_string())
+        );
+        assert_eq!(
+            Some("squanch".to_string()),
+            parse_title(&"<title>\nsquanch</title>".to_string())
+        );
+        assert_eq!(
+            Some("squanch".to_string()),
+            parse_title(&"<title>\n  \n  squanch</title>".to_string())
+        );
+        assert_eq!(
+            Some("we like the moon".to_string()),
+            parse_title(&"<title>\n  \n  we like the moon</title>".to_string())
+        );
+        assert_eq!(
+            Some("&hello123&<>''~".to_string()),
+            parse_title(&"<title>&amp;hello123&amp;&lt;&gt;''~</title>".to_string())
+        );
+        assert_eq!(
+            Some("CVE - CVE-2018-11235".to_string()),
+            parse_title(&"<title>CVE -\nCVE-2018-11235\n</title>".to_string())
+        );
+    }
+
+    #[test]
+    fn parse_images() {
+        let mut opts: ConfOpts = ConfOpts::default();
+        opts.report_metadata = Some(true);
+        match resolve_url("https://rynx.org/sebk/_/DSC_5503.jpg", "en", &opts) {
+            Ok(metadata) => assert_eq!(metadata, "image/jpeg 1000×663"),
+            Err(_) => assert!(false),
+        }
+        match resolve_url(
+            "https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png",
+            "en",
+            &opts,
+        ) {
+            Ok(metadata) => assert_eq!(metadata, "image/png 560×560"),
+            Err(_) => assert!(false),
+        }
+        match resolve_url(
+            "https://upload.wikimedia.org/wikipedia/commons/2/2b/Seven_segment_display-animated.gif",
+            "en",
+            &opts,
+        ) {
+            Ok(metadata) => assert_eq!(metadata, "image/gif 600×752"),
+            Err(_) => assert!(false),
+        }
     }
 }
-
