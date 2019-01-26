a5bf9e35586b005da0d738e65372d6c5527a3cdc
Edward Cragg <drq.11235@gmail.com>
2019-01-26 17:09:11 +0000

    Fix clippy warning clippy::needless_pass_by_value
    warning: this argument is passed by value, but not consumed in the function body
      --> src/message.rs:12:34
       |
    12 |     client: &IrcClient, message: Message, rtd: &Rtd, db: &Database
       |                                  ^^^^^^^
       |
       = note: #[warn(clippy::needless_pass_by_value)] on by default
       = help: for further information visit https://rust-lang-nursery.github.io/rust-clippy/master/index.html#needless_pass_by_value
    help: consider taking a reference instead
       |
    12 |     client: &IrcClient, message: &Message, rtd: &Rtd, db: &Database
    13 | ) {
    14 |     // print the message if debug flag is set
    15 |     if rtd.args.flag_debug {
    16 |         eprintln!("{:?}", message.command)
    17 |     }
     ...

--- src/main.rs
+++ src/main.rs
@@ -127,7 +127,7 @@ fn main() {
 
     // register handler
     reactor.register_client_with_handler(client, move |client, message| {
-        handle_message(client, message, &rtd, &db);
+        handle_message(client, &message, &rtd, &db);
         Ok(())
     });
 
