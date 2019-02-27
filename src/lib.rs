31612eb028fc70b0d6b40fe8e780ef896addaf34
Ed Cragg <drq.11235@gmail.com>
2019-02-27 19:34:45 +0000

    Refactor and use logging provided by log/stderrlog
    Use the `log` and `stderrlog` crates to provide better logging.
    
    Refactor the crate:
     - now uses an internal library (lib.rs)
     - move the main binary to /src/bin/ rather than src
     - Make configuration slightly more sane, such that the runtime data
       struct can now be instantiated using a builder pattern, and is not hard
       coupled to the command line arguments (`Args`) type.

---
+++ src/lib.rs
@@ -0,0 +1,45 @@
+extern crate irc;
+extern crate rusqlite;
+extern crate docopt;
+#[macro_use]
+extern crate serde_derive;
+extern crate itertools;
+extern crate regex;
+#[macro_use]
+extern crate lazy_static;
+#[macro_use]
+extern crate failure;
+extern crate htmlescape;
+extern crate time;
+extern crate reqwest;
+extern crate cookie;
+extern crate image;
+extern crate serde_rusqlite;
+extern crate mime;
+extern crate humansize;
+extern crate unicode_segmentation;
+extern crate toml;
+extern crate directories;
+#[macro_use]
+extern crate log;
+extern crate atty;
+extern crate stderrlog;
+
+pub mod sqlite;
+pub mod http;
+pub mod config;
+pub mod message;
+pub mod buildinfo {
+   include!(concat!(env!("OUT_DIR"), "/built.rs"));
+}
+
+lazy_static! {
+    pub static ref VERSION: String = format!(
+        "v{}{} (build: {})",
+        buildinfo::PKG_VERSION,
+        buildinfo::GIT_VERSION.map_or_else(
+            || String::from(""),
+            |v| format!(" (git {})", v)),
+        buildinfo::PROFILE
+    );
+}
