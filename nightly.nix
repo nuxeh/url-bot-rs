710b85db6eb6461a250cba2eca27f07353162cac
Ed Cragg <ed.cragg@eipi.xyz>
2020-05-16 11:44:55 +0100

    Add stable.nix and nightly.nix

---
+++ nightly.nix
@@ -0,0 +1,24 @@
+# Use latest nightly Rust
+#
+# To use, clone the Mozilla overlay, and provide the path at nix-shell
+# invocation, e.g.:
+#
+#     git clone https://github.com/mozilla/nixpkgs-mozilla.git
+#     nix-shell nightly.nix -I rustoverlay=/path/to/overlay
+#
+# If you want to update Rust versions to the nightly builds, just pull the
+# local overlay repository.
+
+with import <nixpkgs> {};
+with import <rustoverlay/rust-overlay.nix> pkgs pkgs;
+
+stdenv.mkDerivation {
+  name = "url-bot-rs";
+
+  buildInputs = [
+    latest.rustChannels.nightly.rust
+    pkgconfig
+    openssl
+    sqlite
+  ];
+}
